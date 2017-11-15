e78e33d4c8358e0131c320b765a246e339ca4240
cd_huang <cd_huang@gigamore.com>
2017-11-15 14:47:18 +0800

    建立业务线程池处理业务，形成完整的netty线程模型。

--- src/main/java/com/netty4test/rpc/ServerHandler.java
+++ src/main/java/com/netty4test/rpc/ServerHandler.java
@@ -2,43 +2,54 @@ package com.netty4test.rpc;
 
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
-import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.util.concurrent.DefaultThreadFactory;
 
 import java.lang.reflect.Method;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 服务端处理
  * Created by cd_huang on 2017/11/13.
  */
 @ChannelHandler.Sharable
-public class ServerHandler extends ChannelInboundHandlerAdapter {
+public class ServerHandler extends SimpleChannelInboundHandler<RpcRequest> {
+	/**
+	 * 实际业务执行的业务线程池
+	 */
+	private static ThreadPoolExecutor businessExecutor =new ThreadPoolExecutor(100, 500, 60,TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>(10000),new DefaultThreadFactory("businessExecutorForNetty4Test"));
+
 	/**
 	 * 服务端读到客户端消息的处理
 	 * @param ctx
-	 * @param msg
+	 * @param request
 	 * @throws Exception
 	 */
 	@Override
-	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		if(msg instanceof RpcRequest){
-			RpcRequest request =(RpcRequest)msg;
-			RpcResponse response = new RpcResponse(request);
-			try {
-				Class className = request.getInterfaceClass();
-				String methodName = request.getMethodName();
-				Class<?>[] parameterTypes = request.getParameterTypeClass();
-				Object[] arguments = request.getArguments();
-				Object serviceImpl = RpcServer.serviceImplMap.get(className);
-				Method method = serviceImpl.getClass().getMethod(methodName, parameterTypes);
-				Object result = method.invoke(serviceImpl, arguments);
-				response.setResult(result);
-			} catch (Throwable t) {
-				t.printStackTrace();
-				response.setResult(t);
+	protected void channelRead0(final ChannelHandlerContext ctx,final RpcRequest request) throws Exception {
+		businessExecutor.submit(new Runnable(){
+			@Override
+			public void run() {
+				RpcResponse response = new RpcResponse(request);
+				try {
+					Class className = request.getInterfaceClass();
+					String methodName = request.getMethodName();
+					Class<?>[] parameterTypes = request.getParameterTypeClass();
+					Object[] arguments = request.getArguments();
+					Object serviceImpl = RpcServer.serviceImplMap.get(className);
+					Method method = serviceImpl.getClass().getMethod(methodName, parameterTypes);
+					Object result = method.invoke(serviceImpl, arguments);
+					response.setResult(result);
+				} catch (Throwable t) {
+					t.printStackTrace();
+					response.setResult(t);
+				}
+				ctx.channel().writeAndFlush(response);
+				System.out.println("服务端处理请求" + request);
 			}
-			ctx.channel().writeAndFlush(response);
-			System.out.println("服务端处理请求" + request);
-		}
+		});
 	}
 
 	/**
